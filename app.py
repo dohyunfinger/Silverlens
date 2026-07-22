b7fb124b1cbbf2588b7c6094954ff84d491f7de2
c_so0hyuk37 <cshad0700379@gmail.com>
2026-07-23 01:23:50 +0900

    error fixed

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
-from frontend.pages.home import render_home
+from frontend.main_page import render_main_page
 
 
 def main() -> None:
     st.set_page_config(
-        page_title="작품명:SilverLens 팀명:우승 예 동의합니다",
+        page_title="작품명: SilverLens 팀명:우승 예 동의합니다",
         page_icon="✅",
         layout="wide",
     )
@@ -14,7 +14,5 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main(
-    )
-    
-#khjfhgfgvk
+    main()
+#작품명:SilverLens 팀명:우승 예 동의합니다
